af0ce51c71fb17b6d8c5fbadfd88b8a39c9ae21d
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-06-01 18:31:16 +0200

    fix back btn to work on every view

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+import { settingHabit } from "./src/settingHabit.js";
+
 const setHabitBtn = document.querySelector(".set-btn");
 const selectBtn = document.querySelector(".select-btn");
+const backBtns = document.querySelectorAll(".back-btn");
 const startView = document.querySelector(".start-view");
 const settingHabitView = document.querySelector(".setting-habit");
+const addBtn = document.querySelector(".add-btn");
+const habitView = document.querySelector(".week");
 
 const goToSetHabit = () => {
   startView.style.display = "none";
   settingHabitView.style.display = "flex";
 };
 
+const goToStartView = (view) => {
+  view.style.display = "none";
+  startView.style.display = "flex";
+};
+
+const goToHabitView = () => {
+  settingHabitView.style.display = "none";
+  habitView.style.display = "flex";
+};
+
+backBtns.forEach((btn) => {
+  if (btn.parentElement.classList.contains("setting-habit")) {
+    btn.addEventListener("click", () => goToStartView(settingHabitView));
+  }
+  if (btn.parentElement.classList.contains("week")) {
+    btn.addEventListener("click", () => goToStartView(habitView));
+  }
+});
+
 setHabitBtn.addEventListener("click", goToSetHabit);
+// backBtn.addEventListener("click", () => goToStartView(settingHabitView));
+
+addBtn.addEventListener("click", goToHabitView);
---
+++ src/settingHabit.js
@@ -0,0 +1,9 @@
+const habitTitleInput = document.querySelector(".title-input");
+const habitStartDateInput = document.querySelector(".date-input");
+const habitDaysAmountInput = document.querySelector(".days-amount-input");
+
+export const settingHabit = () => {
+  console.log(habitTitleInput.value);
+  console.log(habitStartDateInput.value);
+  console.log(habitDaysAmountInput.value);
+};
